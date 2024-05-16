0577b3aff655025f876d4b99d8a2b42665b4c20e
Aleksey Yuriev <alex11-90@yandex.by>
2024-05-16 14:26:05 +0300

    feat: postList and postForm added

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,50 @@
 <template>
-  <div>
-    <div class="post" v-for="post in posts" :key="post.id">
-      <div><strong>Название:</strong> {{ post.title }}</div>
-      <div><strong>Описание:</strong> {{ post.description }}</div>
-    </div>
+  <div class="app">
+    <post-form @create="createPost" />
+    <post-list :posts="posts" />
   </div>
 </template>
 
 <script>
+import PostForm from '@/components/PostForm.vue';
+import PostList from '@/components/PostList.vue';
+
 export default {
+  components: {
+    PostForm,
+    PostList,
+  },
   data() {
     return {
       likes: 0,
       dislikes: 0,
       posts: [
-        {id: 1, title: 'Пост о Vue', description: 'Прогрессивный JavaScript-фреймворк'},
-        {id: 2, title: 'Пост о React', description: 'Почти JavaScript-фреймворк'},
-        {id: 3, title: 'Пост о Angular', description: 'Фреймворк от Google'}
-      ]
-    }
+        {
+          id: 1,
+          title: 'Пост о Vue',
+          body: 'Прогрессивный JavaScript-фреймворк',
+        },
+        { id: 2, title: 'Пост о React', body: 'Почти JavaScript-фреймворк' },
+        { id: 3, title: 'Пост о Angular', body: 'Фреймворк от Google' },
+      ],
+    };
   },
   methods: {
-    addLike() {
-      this.likes += 1;
+    createPost(post) {
+      this.posts.push(post);
     },
-    addDislike() {
-      this.dislikes += 1;
-    }
-  }
-}
+  },
+};
 </script>
 
 <style>
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
-  .post {
-    padding: 15px;
-    border: 2px solid teal;
-    margin-top: 15px;
-  }
+.app {
+  padding: 20px;
+}
 </style>
