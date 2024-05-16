48289b3eff75ce4a41bbd4d1b71bb9aa465ab308
Aleksey Yuriev <alex11-90@yandex.by>
2024-05-16 15:29:13 +0300

    feat: UI my-button and my-input added

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -1,24 +1,21 @@
 <template>
   <form @submit.prevent>
     <h4>Создание поста</h4>
-    <input
-      v-model="post.title"
-      class="input"
-      type="text"
-      placeholder="Название"
-    />
-    <input
-      v-model="post.body"
-      class="input"
-      type="text"
-      placeholder="Описание"
-    />
-    <button class="btn" @click="createPost">Создать</button>
+    <my-input v-model="post.title" type="text" placeholder="Название" />
+    <my-input v-model="post.body" type="text" placeholder="Описание" />
+    <my-button
+      class="btn"
+      @click="createPost"
+      style="align-self: flex-end; margin-top: 15px"
+      >Создать</my-button
+    >
   </form>
 </template>
 
 <script>
+import MyInput from './UI/MyInput.vue';
 export default {
+  components: { MyInput },
   data() {
     return {
       post: {
@@ -44,21 +41,5 @@ export default {
 form {
   display: flex;
   flex-direction: column;
-}
-
-.input {
-  width: 100%;
-  border: 1px solid teal;
-  padding: 10px 15px;
-  margin-top: 15px;
-}
-
-.btn {
-  margin-top: 15px;
-  align-self: flex-end;
-  padding: 10px 15px;
-  background: none;
-  color: teal;
-  border: 1px solid teal;
 }
 </style>
